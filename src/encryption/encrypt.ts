e0ccb8a3d7c5ed6c282d813141c819c906e9247b
Malik Elbadri <46916173+Maliklar@users.noreply.github.com>
2024-12-27 12:20:17 +0300

    Updated transactions

--- src/encryption/encrypt.ts
+++ src/encryption/encrypt.ts
@@ -1,9 +1,16 @@
-import { generateKeyPairSync } from "crypto";
-export default async function encrypt(message: string) {
-  const keyPair = generateKeyPairSync("rsa", {
+import crypto from "crypto";
+export default async function encrypt(data: string) {
+  const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
     modulusLength: 2048,
     publicKeyEncoding: { type: "pkcs1", format: "pem" },
     privateKeyEncoding: { type: "pkcs1", format: "pem" },
   });
-  console.log(keyPair);
+  const encryptedData = crypto.publicEncrypt(publicKey, Buffer.from(data));
+  console.log("Encrypted Data:", encryptedData.toString("base64"));
+
+  return {
+    data,
+    publicKey,
+    privateKey,
+  };
 }
